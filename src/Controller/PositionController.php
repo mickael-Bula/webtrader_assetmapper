dd86266450653c867e785af1ad190d9fe32d2bc0
Locullus <bula.mickael@gmail.com>
2026-03-06 18:57:58 +0100

    Ajoute un bouton pour créer des positions en cours ou en attente

---
+++ src/Controller/PositionController.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Position;
+use App\Repository\PositionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PositionController extends AbstractController
+{
+    /**
+     * @throws \Exception
+     */
+    #[Route('/position/create', name: 'app_position_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // 1. Récupération des données du formulaire
+        $quantity = $request->request->get('quantity');
+        $buyPriceCac = $request->request->get('buy_price_cac');
+        $buyPriceLvc = $request->request->get('buy_price_lvc');
+        $targetPriceCac = $request->request->get('target_price_cac');
+        $targetPriceLvc = $request->request->get('target_price_lvc');
+        $validityDate = $request->request->get('validity_date');
+
+        // 2. Création de l'entité
+        $position = new Position();
+//        $position->setQuantity((int)$quantity);
+        $position->setBuyPrice((string)$buyPriceCac);
+        $position->setLvcBuyPrice((string)$buyPriceLvc);
+        $position->setTargetPrice((string)$targetPriceCac);
+        $position->setLvcTargetPrice((string)$targetPriceLvc);
+
+        // Gestion de la date
+//        if ($validityDate) {
+//            $position->setValidityDate(new \DateTime($validityDate));
+//        }
+
+        // Il faut définir le statut de la position en fonction du type créé
+        // $position->setStatus('waiting');
+
+        // 3. Sauvegarde
+        $entityManager->persist($position);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'La nouvelle position a été enregistrée avec succès.');
+
+        // 4. Redirection vers le dashboard
+        return $this->redirectToRoute('app_home');
+    }
+}
